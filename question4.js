8bfc1bfe6ee6af9b68264e0e94c85c718ab37e96
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-02-18 13:26:42 -0500

    i want to be stricly used for the last 2 extra credit points Please enter the commit message for your changes. Lines starting

---
+++ question4.js
@@ -0,0 +1,19 @@
+//Ryan POstma
+//CMP237
+//question 4
+
+//create a basic function
+function doSomthing(number) {//pass it a parameter
+	var count = number;//add the parameter to count
+	function doSomthingElse() {//give another function inside of the first one
+		var exclusiveCount = count * 5;//create a new variable to show nested scope and give it a number
+		return exlusiveCount;//return nest scope varialbe
+	}//end doSomthingElse
+	return count = exclusiveCount;//this line will not be able to find exlusiveCount due to it being nested in the funtion above
+}//end doSomthing
+
+//make a random variable
+var x = 2;
+
+//run function
+print(doSomthing(x));
