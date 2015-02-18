d9f362c10cad2523c84fec18b71f38a55fbd16f1
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-02-18 12:44:26 -0500

    question 1

---
+++ question1.js
@@ -0,0 +1,8 @@
+var x = 10;//create a variable and give it a value
+
+function edit(number) {//change the value of x
+	x = number * 10;//multiply by 10
+	return x;//return the value
+}
+
+print(edit(10));//print the result
